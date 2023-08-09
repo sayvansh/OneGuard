f77a27fcdf64ee567cc83893c6c1495cd5011df7
s.kalami <s.kalami@tes.ir>
2023-08-09 13:26:17 +0330

    bugfix : appsettings.json bug fixed. migrations added.

---
+++ OneGuard/Migrations/20230809083129_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "endpoint",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    url = table.Column<string>(type: "text", nullable: false),
+                    otp_ttl = table.Column<int>(type: "integer", nullable: false),
+                    secret_ttl = table.Column<int>(type: "integer", nullable: false),
+                    length = table.Column<int>(type: "integer", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_endpoint", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "endpoint");
+        }
+    }
+}
